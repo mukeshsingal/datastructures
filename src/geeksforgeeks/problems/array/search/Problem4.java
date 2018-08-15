eb52a161735f82a18f34fa8d1167c583884db4e8
Mukesh Singal <mukesh.singal@kony.com>
2018-08-15 22:51:11 +0530

    Largest Sum subarray Kadane's Algo

---
+++ src/geeksforgeeks/problems/array/search/Problem4.java
@@ -0,0 +1,22 @@
+package geeksforgeeks.problems.array.search;
+
+/* Largest Sum subarray */
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] array = {1, -8, 1, -2, 3, -2, 2};
+        maximumSumSubArray(array);
+    }
+
+    /* Kadane's Algorithm */
+    public static void maximumSumSubArray(int[] arr) {
+        int global_max, current_max;
+        global_max = current_max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            current_max = Math.max(arr[i], current_max + arr[i]);
+            if (current_max > global_max) {
+                global_max = current_max;
+            }
+        }
+        System.out.println(global_max);
+    }
+}
