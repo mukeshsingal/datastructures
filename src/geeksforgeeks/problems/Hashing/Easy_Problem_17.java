e280d4b88b79238cf3bb5dfb0fb5aa9a14daadd8
Mukesh Singal <mukesh.singal@kony.com>
2018-10-01 08:37:39 +0530

    Hashing: Problem 17 Maximum distance between two occurrences of same element in array

---
+++ src/geeksforgeeks/problems/Hashing/Easy_Problem_17.java
@@ -0,0 +1,42 @@
+package geeksforgeeks.problems.Hashing;
+
+import java.util.HashMap;
+
+/**
+ * Maximum distance between two occurrences of same element in array
+ *
+ * Given an array with repeated elements, the task is to find the maximum distance two occurrences of an element.
+ *
+ * Examples:
+ *
+ * Input : arr[] = {3, 2, 1, 2, 1, 4, 5, 8, 6, 7, 4, 2}
+ * Output: 10
+ * // maximum distance for 2 is 11-1 = 10
+ * // maximum distance for 1 is 4-2 = 2
+ * // maximum distance for 4 is 10-5 = 5
+ *
+ */
+public class Easy_Problem_17 {
+    public static void main(String[] args) {
+        int arr[] = {3, 2, 1, 2, 1, 4, 5, 8, 6, 7, 4, 2};
+
+        HashMap<Integer, Integer> firstIndices = new HashMap<>();
+
+        int maximumDistance = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(!firstIndices.containsKey(arr[i])){
+                firstIndices.put(arr[i], i);
+            }
+            else {
+                int temp = i - firstIndices.get(arr[i]);
+                if(temp > maximumDistance) {
+                    maximumDistance = temp;
+                }
+            }
+        }
+
+        System.out.println("Maximum distance is " + maximumDistance);
+
+    }
+}
---
+++ src/geeksforgeeks/problems/Hashing/Main.java
@@ -0,0 +1,10 @@
+package geeksforgeeks.problems.Hashing;
+
+public class Main {
+    /*
+     * Maximum distance between two occurrences of same element in array
+
+     *
+     *
+     */
+}
