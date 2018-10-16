41ed35f51e8c40c3ff8e07cf7fd16a20e64461db
Mukesh Singal <mukesh.singal@kony.com>
2018-10-16 21:50:38 +0530

    Tree: problem 40. Print all nodes that don’t have sibling

--- src/geeksforgeeks/problems/Tree/Traversal/BinaryTreeUtil.java
+++ src/geeksforgeeks/problems/Tree/Traversal/BinaryTreeUtil.java
@@ -34,7 +34,8 @@ public class BinaryTreeUtil {
         root.left = new Node<>(2);
         root.right = new Node<>(3);
         root.left.left = new Node<>(4);
-        root.left.right = new Node<>(5);
+        root.right.left = new Node<>(5);
+        //root.left.right = new Node<>(5);
         return root;
     }
 
