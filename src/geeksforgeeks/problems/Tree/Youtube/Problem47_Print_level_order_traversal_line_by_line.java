a142d5f54f45256bc1484343df1fce5dd0e3f03b
Mukesh Singal <mukesh.singal@kony.com>
2018-10-17 10:03:18 +0530

    Tree Problem 47: Print level order traversal line by line

---
+++ src/geeksforgeeks/problems/Tree/Youtube/Problem47_Print_level_order_traversal_line_by_line.java
@@ -0,0 +1,9 @@
+package geeksforgeeks.problems.Tree.Youtube;
+
+import geeksforgeeks.problems.Tree.BinaryTreeUtil;
+
+public class Problem47_Print_level_order_traversal_line_by_line extends BinaryTreeUtil {
+    public static void main(String[] args) {
+        levelOrderTraversal(getBinaryTree());
+    }
+}
