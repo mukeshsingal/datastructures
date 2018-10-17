65f4d1d7439e389a21db63da7904e65e11c77418
Mukesh Singal <mukesh.singal@kony.com>
2018-10-17 06:37:36 +0530

    Tree: problem 44: Foldable Binary Trees

---
+++ src/geeksforgeeks/problems/Tree/NamedProblems/Problem44_Foldable_Binary_Trees.java
@@ -0,0 +1,19 @@
+package geeksforgeeks.problems.Tree.NamedProblems;
+
+import geeksforgeeks.problems.Tree.Traversal.BinaryTreeUtil;
+
+public class Problem44_Foldable_Binary_Trees extends BinaryTreeUtil {
+    public static void main(String[] args) {
+        Node node = getBinaryTree();
+
+        //Here tree will be foldable if left and right subtree are same in structure
+        System.out.println("Tree is " + (isFoldable(node.left, node.right) ? "Foldable" : "Not Foldable"));
+    }
+
+    static boolean isFoldable(Node root1, Node root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+
+        return isFoldable(root1.left, root2.right) && isFoldable(root1.right, root2.left);
+    }
+}
--- src/geeksforgeeks/problems/Tree/Traversal/BinaryTreeUtil.java
+++ src/geeksforgeeks/problems/Tree/Traversal/BinaryTreeUtil.java
@@ -33,8 +33,8 @@ public class BinaryTreeUtil {
         Node root = new Node<>(1);
         root.left = new Node<>(2);
         root.right = new Node<>(3);
-        root.left.left = new Node<>(4);
-        root.right.left = new Node<>(5);
+        /*root.left.left = new Node<>(4);
+        root.right.left = new Node<>(5);*/
         return root;
     }
 
