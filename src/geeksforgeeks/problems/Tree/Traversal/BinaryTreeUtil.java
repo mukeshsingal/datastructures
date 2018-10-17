ddf94b01209af29578e1f10ae101d4938b3131a3
Mukesh Singal <mukesh.singal@kony.com>
2018-10-17 09:22:45 +0530

    Tree Problem 45: Find mirror of a given node in Binary tree

--- src/geeksforgeeks/problems/Tree/Traversal/BinaryTreeUtil.java
+++ src/geeksforgeeks/problems/Tree/Traversal/BinaryTreeUtil.java
@@ -17,7 +17,7 @@ public class BinaryTreeUtil {
 
         @Override
         public String toString() {
-            return key.toString();
+            return String.valueOf(key);
         }
     }
 
@@ -33,8 +33,8 @@ public class BinaryTreeUtil {
         Node root = new Node<>(1);
         root.left = new Node<>(2);
         root.right = new Node<>(3);
-        /*root.left.left = new Node<>(4);
-        root.right.left = new Node<>(5);*/
+        root.left.left = new Node<>(4);
+        root.right.right = new Node<>(5);
         return root;
     }
 
