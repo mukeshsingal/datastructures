aa41c22511da94ec56eb366ca68a11bdb338850f
Mukesh Singal <mukesh.singal@kony.com>
2018-10-17 15:08:30 +0530

    Tree Problem 51: Boundary Traversal of binary tree

--- src/geeksforgeeks/problems/Tree/BinaryTreeUtil.java
+++ src/geeksforgeeks/problems/Tree/BinaryTreeUtil.java
@@ -38,6 +38,16 @@ public class BinaryTreeUtil {
         return root;
     }
 
+    /**
+     *
+     * 1       1
+     *      /   \
+     *     2     3
+     *    / \    / \
+     *   4   5  6   7
+     *      / \
+     *      8  9
+     */
     public static Node getBinaryTree2() {
         Node root = new Node<>(1);
         root.left = new Node<>(2);
@@ -46,6 +56,9 @@ public class BinaryTreeUtil {
         root.left.right = new Node<>(5);
         root.right.left = new Node<>(6);
         root.right.right = new Node<>(7);
+        root.left.right.left = new Node<>(8);
+        root.left.right.right = new Node<>(9);
+
 
         return root;
     }
