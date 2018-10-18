a802d82470e71271d4d922977d7025f9c199f0d3
Mukesh Singal <mukesh.singal@kony.com>
2018-10-18 06:53:23 +0530

    Tree Problem 54. Check for Children Sum Property in a Binary Tree

--- src/geeksforgeeks/problems/Tree/BinaryTreeUtil.java
+++ src/geeksforgeeks/problems/Tree/BinaryTreeUtil.java
@@ -30,11 +30,11 @@ public class BinaryTreeUtil {
      * 4   5
      */
     public static Node getBinaryTree() {
-        Node root = new Node<>(1);
-        root.left = new Node<>(2);
-        root.right = new Node<>(3);
-        root.left.left = new Node<>(4);
-        root.left.right = new Node<>(5);
+        Node root = new Node<>(8);
+        root.left = new Node<>(3);
+        root.right = new Node<>(5);
+        root.left.left = new Node<>(3);
+        root.right.right = new Node<>(5);
         return root;
     }
 
