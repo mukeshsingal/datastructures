3def0504c07152a2ae8310030f7f2bba2a8d43b0
mukeshsignal <mukesh.singal@outlook.com>
2019-06-03 09:00:04 +0530

    Create Problem_538_Convert_BST_to_Greater_Tree.java

---
+++ src/main/java/leetcode/Problem_538_Convert_BST_to_Greater_Tree.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST
+ * is changed to the original key plus sum of all keys greater than the original key in BST.
+ */
+
+public class Problem_538_Convert_BST_to_Greater_Tree {
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
