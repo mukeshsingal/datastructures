9ad01ad215c2733dcfeb194b1d2441b8d0128016
mukeshsignal <mukesh.singal@outlook.com>
2019-06-03 09:01:47 +0530

    Create Problem_494_Target_Sum.java

---
+++ src/main/java/leetcode/Problem_494_Target_Sum.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a target, S.
+ * Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+ *
+ * Find out how many ways to assign symbols to make sum of integers equal to target S.
+ *
+ */
+public class Problem_494_Target_Sum {
+    int count = 0;
+    public int findTargetSumWays(int[] nums, int S) {
+        int[][] memo = new int[nums.length][2001];
+        for (int[] row: memo)
+            Arrays.fill(row, Integer.MIN_VALUE);
+        return calculate(nums, 0, 0, S, memo);
+    }
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S)
+                return 1;
+            else
+                return 0;
+        } else {
+            if (memo[i][sum + 1000] != Integer.MIN_VALUE) {
+                return memo[i][sum + 1000];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + 1000] = add + subtract;
+            return memo[i][sum + 1000];
+        }
+    }
+}
