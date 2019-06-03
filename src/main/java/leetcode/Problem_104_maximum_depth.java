e1ea15ad58dcd379f3cdae90e68d7e0a663fe9c5
mukeshsignal <mukesh.singal@outlook.com>
2019-06-03 12:36:17 +0530

    Create Problem_104_maximum_depth.java

---
+++ src/main/java/leetcode/Problem_104_maximum_depth.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Problem_104_maximum_depth {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if(root.left == null && root.right == null) {
+            return 1;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
