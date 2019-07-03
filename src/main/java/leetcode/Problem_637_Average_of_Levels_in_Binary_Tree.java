b470571fef8494b8706ad023592f865f41d708ac
mukeshsignal <mukesh.singal@outlook.com>
2019-07-03 07:42:51 +0530

    Create Problem_637_Average_of_Levels_in_Binary_Tree.java

---
+++ src/main/java/leetcode/Problem_637_Average_of_Levels_in_Binary_Tree.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Problem_637_Average_of_Levels_in_Binary_Tree {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for(int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if(node.left != null)
+                    queue.add(node.left);
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+            result.add(sum/size);
+        }
+        return result;
+    }
+}
