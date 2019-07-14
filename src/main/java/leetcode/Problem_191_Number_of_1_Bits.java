02aebcd0aa2790e88f2f58d1a7abce8ca688ffa4
mukeshsignal <mukesh.singal@outlook.com>
2019-07-14 21:35:56 +0530

    Create Problem_191_Number_of_1_Bits.java

---
+++ src/main/java/leetcode/Problem_191_Number_of_1_Bits.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Problem_191_Number_of_1_Bits {
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
