41da2d97c46f2fa1db3bbbd9b9babd3afc1be8aa
mukeshsignal <mukesh.singal@outlook.com>
2019-07-17 21:28:38 +0530

    Create Problem_461_Hamming_Distance.java

---
+++ src/main/java/leetcode/Problem_461_Hamming_Distance.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class Problem_461_Hamming_Distance {
+
+    public int hammingDistance(int x, int y) {
+        int hDistance = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int a = x & 1;
+            int b = y & 1;
+            if(a != b) {
+                hDistance++;
+            }
+            x=x>>1;
+            y=y>>1;
+        }
+        return hDistance
+    }
+}
