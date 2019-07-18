c0c02772b628670321a5793ec9b2dce0de17e8d8
mukeshsignal <mukesh.singal@outlook.com>
2019-07-18 09:18:04 +0530

    Create Problem_342_Power_of_Four.java

---
+++ src/main/java/leetcode/Problem_342_Power_of_Four.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+public class Problem_342_Power_of_Four {
+    
+    public boolean isPowerOfFour(int num) {
+        return (num > 0) && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
+    }
+}
