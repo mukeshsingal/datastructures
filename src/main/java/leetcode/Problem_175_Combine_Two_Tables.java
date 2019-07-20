f18d1940675144aa8af9b9f204a512547e962d53
mukeshsignal <mukesh.singal@outlook.com>
2019-07-20 21:22:55 +0530

    Create Problem_175_Combine_Two_Tables.java

---
+++ src/main/java/leetcode/Problem_175_Combine_Two_Tables.java
@@ -0,0 +1,6 @@
+package leetcode;
+
+public class Problem_175_Combine_Two_Tables {
+    //SELECT FirstName, LastName, City, State FROM Person
+    //LEFT JOIN Address ON Person.PersonId = Address.PersonId
+}
