28c9b9e65a3b23234b739f774cfcde60265d2ea7
mukeshsignal <mukesh.singal@outlook.com>
2020-04-25 19:25:09 +0530

    Commit 7 - Apr 25

---
+++ src/main/java/leetcode/Problem_921_Minimum_Add_to_Make_Parentheses_Valid.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class Problem_921_Minimum_Add_to_Make_Parentheses_Valid {
+
+}
+class Solution {
+    public int minAddToMakeValid(String S) {
+        char[] arr = S.toCharArray();
+        
+        int ans = 0;
+        int count = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == '(') {
+                count++;
+            }
+            else {
+                if(count == 0) {
+                    ans++;
+                }
+                else {
+                    count--;
+                }
+            }
+        }
+        
+        for(int i = arr.length-1; i>=0; i--) {
+            if(arr[i] == '(' && count != 0) {
+                ans++;
+                count--;
+            }
+        }
+        return ans;
+    }
+}
