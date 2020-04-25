9fb6a643a9ade18ed96b879cfc34a00ee90ba655
mukeshsignal <mukesh.singal@outlook.com>
2020-04-25 19:24:44 +0530

    Commit 3 - Apr 25

---
+++ src/main/java/leetcode/Problem_1249_Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+public class Problem_1249_Minimum_Remove_to_Make_Valid_Parentheses {
+
+}
+
+class Solution {
+    
+    /* 1249. Minimum Remove to Make Valid Parentheses */
+    /*
+        Algorithm:
+        Keep two stacks. 1. Keep the parenthesis and other the index from string
+        if balance remove from both the stacks
+    */
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        
+        Stack<Character> parenthesisStack = new Stack<>();
+        Stack<Integer> indexStack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c >= 'a' && c <= 'z') continue;
+            
+            if(parenthesisStack.isEmpty()) {
+                parenthesisStack.push(c);
+                indexStack.push(i);
+            }
+            else {
+                char top = parenthesisStack.peek();
+                if(c == ')' && top == '(') {
+                    parenthesisStack.pop();
+                    indexStack.pop();
+                }
+                else {
+                   parenthesisStack.push(c);
+                   indexStack.push(i); 
+                }
+            }
+        }
+        while(!parenthesisStack.isEmpty()) {
+            parenthesisStack.pop();
+            int index = indexStack.pop();
+            sb.deleteCharAt(index);
+        }
+        return sb.toString();
+    }
+}
