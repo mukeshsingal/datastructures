000920632636a2b4acaae1af25684260c014c0f5
mukeshsignal <mukesh.singal@outlook.com>
2020-04-25 19:25:16 +0530

    Commit 8 - Apr 25

---
+++ src/main/java/leetcode/Problem_946_Validate_Stack_Sequences.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class Problem_946_Validate_Stack_Sequences {
+
+}
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0;
+        int j = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        while (j != popped.length) {
+            if(i < pushed.length && popped[j] == pushed[i]) {
+                i++;
+                j++; 
+                continue;
+            }
+            if(stack.isEmpty() || popped[j] != stack.peek()) {
+                if(i == pushed.length) {
+                    return false;
+                }
+                stack.push(pushed[i]);
+                i++;
+            }
+            else {
+                stack.pop();
+                j++;
+            }
+        }
+        return true;
+    }
+}
