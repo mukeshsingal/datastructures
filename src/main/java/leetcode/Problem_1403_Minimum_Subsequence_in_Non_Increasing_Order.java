f9416d25ec2c9a08ac0028c3fa03dd72dcab6699
mukeshsignal <mukesh.singal@outlook.com>
2020-04-26 20:19:55 +0530

    Create Problem_1403_Minimum_Subsequence_in_Non_Increasing_Order.java

---
+++ src/main/java/leetcode/Problem_1403_Minimum_Subsequence_in_Non_Increasing_Order.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+public class Problem_1403_Minimum_Subsequence_in_Non_Increasing_Order {
+}
+
+class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+
+        List<Integer> res = new ArrayList<>();
+        int[] freq = new int[101];
+
+        int sum = 0;
+        int curSum = 0;
+        for (int num:nums) {
+            sum += num;
+            freq[num]++;
+        }
+        for (int i = 100; i > 0; i--) {
+            if (freq[i] > 0) {
+                curSum += i;
+                res.add(i);
+                freq[i]--;
+                if (curSum > sum - curSum)
+                    return res;
+                else {
+                    i++;
+                }
+            }
+        }
+        return res;
+    }
+}
