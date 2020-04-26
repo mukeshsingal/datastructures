3db76b80b533abb90eb475380ad45e3701881660
mukeshsignal <mukesh.singal@outlook.com>
2020-04-26 20:19:43 +0530

    Create Problem_1029_Two_City_Scheduling.java

---
+++ src/main/java/leetcode/Problem_1029_Two_City_Scheduling.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class Problem_1029_Two_City_Scheduling {
+
+}
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o1[1] - (o2[0] - o2[1]);
+            }
+        });
+        int aPending = costs.length / 2;
+        int bPending = costs.length / 2;
+        int minCost = 0;
+        for(int i = 0; i < costs.length; i++) {
+            if(i < costs.length / 2) {
+                minCost += costs[i][0];
+            } else {
+                minCost += costs[i][1];
+            }
+        }
+        return minCost;
+    }
+}
