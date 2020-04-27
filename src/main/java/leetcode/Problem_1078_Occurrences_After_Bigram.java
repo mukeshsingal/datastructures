36ffe460cfe70e9cb6331796e9060d7d317a5615
mukeshsignal <mukesh.singal@outlook.com>
2020-04-27 22:10:30 +0530

    Create Problem_1078_Occurrences_After_Bigram.java

---
+++ src/main/java/leetcode/Problem_1078_Occurrences_After_Bigram.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Problem_1078_Occurrences_After_Bigram {
+
+}
+
+class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+        String[] words = text.split(" ");
+        List<String> list = new ArrayList<String>();
+        
+        for(int i=0;i<words.length-2;i++){
+            if(words[i].equals(first) && words[i+1].equals(second))
+                list.add(words[i+2]);
+        }
+        return list.toArray(new String[0]);        
+    }
+}
