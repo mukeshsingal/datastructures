efb5d9bfcf1b8fb486963be1bf48934f346df0be
mukeshsignal <mukesh.singal@outlook.com>
2020-04-27 21:16:59 +0530

    Create Problem_1207_Unique_Number_of_Occurrence.java

---
+++ src/main/java/leetcode/Problem_1207_Unique_Number_of_Occurrence.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class Problem_1207_Unique_Number_of_Occurrence {
+
+}
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int[] hashmap1 = new int[2001];
+        int[] hashmap2 = new int[1001];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for(int i : arr) {
+            hashmap1[i + 1000]++;
+            if (i + 1000 < min) {
+                min = i + 1000;
+            }
+            
+            if (i + 1000 > max) {
+                max = i + 1000;
+            }
+        }
+         for (int i = min; i <= max; i++) {
+            if(hashmap1[i] != 0) {
+                if(hashmap2[hashmap1[i]] == -2) {
+                    return false;
+                }
+                hashmap2[hashmap1[i]] = -2;    
+            }
+        }
+        
+        return true;
+    }
+}
