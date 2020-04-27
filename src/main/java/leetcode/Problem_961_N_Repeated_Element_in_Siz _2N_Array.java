751912cf14c9ae403bff1da3946afcf633fba013
mukeshsignal <mukesh.singal@outlook.com>
2020-04-27 21:17:34 +0530

    Create Problem_961_N_Repeated_Element_in_Siz _2N_Array.java

---
+++ src/main/java/leetcode/Problem_961_N_Repeated_Element_in_Siz _2N_Array.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Problem_961_N_Repeated_Element_in_Siz_2N_Array {
+
+}
+
+class Solution {
+    public int repeatedNTimes(int[] A) {
+     int[] arr = new int[10000];   
+        for(int i: A) {
+            if(arr[i] == -1) {
+                return i;
+            }
+            arr[i] = -1;
+        }
+        return -1;
+    }
+}
