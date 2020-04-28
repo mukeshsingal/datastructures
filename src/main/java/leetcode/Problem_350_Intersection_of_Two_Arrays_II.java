d76e09a5afc6e7ba363d2d6bc81eeb382e65be91
mukeshsignal <mukesh.singal@outlook.com>
2020-04-28 23:30:05 +0530

    Create Problem_350_Intersection_of_Two_Arrays_II.java

---
+++ src/main/java/leetcode/Problem_350_Intersection_of_Two_Arrays_II.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class Problem_350_Intersection_of_Two_Arrays_II {
+
+}
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        ArrayList<Integer> list = new ArrayList();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i: nums1) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        for(int i: nums2) {
+            if(map.containsKey(i) && map.get(i) > 0) {
+                map.put(i, map.get(i)-1);
+                list.add(i);
+            }
+        }
+        int size = list.size();
+        int[] result = new int[size];
+        int index = 0;
+        for (Integer i : list) {
+            result[index] = i;
+            index++;
+        }
+        return result;
+    }
+}
