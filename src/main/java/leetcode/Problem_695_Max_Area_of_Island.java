d05788fc24343c6b876648dcbabf230d67b4cfb3
mukeshsignal <mukesh.singal@outlook.com>
2020-05-01 11:15:08 +0530

    Create Problem_695_Max_Area_of_Island.java

---
+++ src/main/java/leetcode/Problem_695_Max_Area_of_Island.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class Problem_695_Max_Area_of_Island {
+
+}
+
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+        
+        int max = 0;
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(grid[i][j] == 1) {
+                    max = Math.max(max, DFS(grid, i, j));
+                }
+            }
+        }
+        return max;
+    }
+    
+    public int DFS(int[][] grid, int i, int j) {
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        
+        return 1 + DFS(grid, i + 1, j) + DFS(grid, i - 1, j) + DFS(grid, i, j - 1) + DFS(grid, i, j + 1);
+    }
+}
