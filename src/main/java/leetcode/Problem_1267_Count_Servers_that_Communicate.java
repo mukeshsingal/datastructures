211716cc8d583490411340bd0abbe18a2bcb749d
mukeshsignal <mukesh.singal@outlook.com>
2020-05-01 12:12:51 +0530

    Create Problem_1267_Count_Servers_that_Communicate.java

---
+++ src/main/java/leetcode/Problem_1267_Count_Servers_that_Communicate.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class Problem_1267_Count_Servers_that_Communicate {
+
+}
+
+class Solution {
+    public int countServers(int[][] grid) {
+        int count = 0;
+        int[] rows = new int[grid.length];
+        int[] cols = new int[grid[0].length];
+        
+        for(int i = 0; i < grid.length; i++) {
+            int serverCount = 0; 
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    rows[i]++;
+                    cols[j]++;
+                }
+            }
+        }
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1 && (rows[i] > 1 || cols[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
