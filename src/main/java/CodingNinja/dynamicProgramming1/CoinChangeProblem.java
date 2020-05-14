3f87b8aa2c0cc3985a3dcee266e068c0192ceb89
mukeshsignal <mukesh.singal@outlook.com>
2020-05-14 16:48:02 +0530

    Update CoinChangeProblem.java

--- src/main/java/CodingNinja/dynamicProgramming1/CoinChangeProblem.java
+++ src/main/java/CodingNinja/dynamicProgramming1/CoinChangeProblem.java
@@ -1,37 +1,36 @@
 package CodingNinja.dynamicProgramming1;
 
 import java.util.Arrays;
+
 public class CoinChangeProblem {
 
-
-    public static int countWaysToMakeChange(int denominations[], int value){
-        int[][] matrix = new int[value+1][denominations.length];
-        for(int[] a : matrix) {
+    public static int countWaysToMakeChange(int[] denominations, int value) {
+        int[][] matrix = new int[value + 1][denominations.length];
+        for (int[] a : matrix) {
             Arrays.fill(a, -1);
         }
-
 
         return ways(denominations, value, 0, matrix);
     }
 
-    public static int ways(int denominations[], int value, int dCount, int[][] matrix) {
-        if(value == 0) {
+    public static int ways(int[] denominations, int value, int dCount, int[][] memo) {
+        if (value == 0) {
             return 1;
         }
-        if(value < 0) {
+        if (value < 0) {
             return 0;
         }
-        if(dCount == denominations.length) {
+        if (dCount == denominations.length) {
             return 0;
         }
-        if(matrix[value][dCount] > -1) {
-            return matrix[value][dCount];
+        if (memo[value][dCount] > -1) {
+            return memo[value][dCount];
         }
 
-        int first = ways(denominations, value - denominations[dCount], dCount, matrix);
-        int second = ways(denominations, value, dCount + 1, matrix);
+        int first = ways(denominations, value - denominations[dCount], dCount, memo);
+        int second = ways(denominations, value, dCount + 1, memo);
 
-        matrix[value][dCount] = first + second;
+        memo[value][dCount] = first + second;
         return first + second;
     }
 }
