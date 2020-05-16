49f1bde83cf8cab012dbbe52d1390ec85383661a
mukeshsignal <mukesh.singal@outlook.com>
2020-05-16 10:19:07 +0530

    Create ConstructArray.java

---
+++ src/main/java/CodingNinja/DynamicProgramming1/ConstructArray.java
@@ -0,0 +1,24 @@
+package CodingNinja.DynamicProgramming1;
+
+/**
+ * Find number of ways to contruct an array such that consecutive positions contains different values
+ * 1. Array contains n elements.
+ * 2. first element will be 1 and last will be x
+ * 3. each element value will be between 1 and k 
+ * Eg.
+ * n = 4, k = 3, x = 2;
+ * Ans: 3 ways. 
+ * 1 2 1 2
+ * 1 2 3 2
+ * 1 3 1 2
+ */
+public class ConstructArray {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static int numberOfWays(int n, int k, int x) {
+        return 0;
+    }
+}
+
