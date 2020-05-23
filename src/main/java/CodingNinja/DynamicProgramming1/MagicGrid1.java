d9b6a9c46d0c2632f173b287294935f0d4a6b085
mukeshsignal <mukesh.singal@outlook.com>
2020-05-23 22:53:49 +0530

    Create MagicGrid1.java

---
+++ src/main/java/CodingNinja/DynamicProgramming1/MagicGrid1.java
@@ -0,0 +1,19 @@
+package CodingNinja.DynamicProgramming1;
+
+public class MagicGrid1 {
+
+    public static void main(String[] args) {
+        
+    }
+    public static int minCostGrid(int[] grid, int si, int sj, int ei, int ej) {
+        
+        
+        if(si == ei && sj == ej) {
+            return 0;
+        }
+        return 0;
+
+
+    }
+}
+
