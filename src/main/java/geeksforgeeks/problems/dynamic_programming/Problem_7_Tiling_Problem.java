32b3d5dac3bdca33acd611ca812e1d5e7302a5b0
mukeshsignal <mukesh.singal@outlook.com>
2020-06-09 08:57:33 +0530

    Create Problem_7_Tiling_Problem.java

---
+++ src/main/java/geeksforgeeks/problems/dynamic_programming/Problem_7_Tiling_Problem.java
@@ -0,0 +1,11 @@
+package geeksforgeeks.problems.dynamic_programming;
+
+/**
+ * Tiling Problem
+ * Let “count(n)” be the count of ways to place tiles on a “2 x n” grid, we have following two ways to place first tile.
+ * 1) If we place first tile vertically, the problem reduces to “count(n-1)”
+ * 2) If we place first tile horizontally, we have to place second tile also horizontally. So the problem reduces to “count(n-2)”
+ */
+public class Problem_7_Tiling_Problem {
+    
+}
