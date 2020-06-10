6c3132fd634ffa8405bdda136df1746dafbeb10c
mukeshsignal <mukesh.singal@outlook.com>
2020-06-10 09:30:00 +0530

    Update Problem_9_CoinChange.java

--- src/main/java/geeksforgeeks/problems/dynamic_programming/Problem_9_CoinChange.java
+++ src/main/java/geeksforgeeks/problems/dynamic_programming/Problem_9_CoinChange.java
@@ -2,7 +2,7 @@ package geeksforgeeks.problems.dynamic_programming;
 
 public class Problem_9_CoinChange {
     public static void main(String[] args) {
-        System.out.println(coinChnage(new int[]{1,2,3}, 3, 4));
+        System.out.println(coinChnage(new int[] { 1, 2, 3 }, 3, 4));
     }
 
     public static int coinChnage(int[] coins, int i, int N) {
@@ -12,10 +12,10 @@ public class Problem_9_CoinChange {
         if (N < 0)
             return 0;
 
-            if (i <=0 && N >= 1) 
-            return 0; 
-        
-        return coinChnage(coins, i-1,  N) + coinChnage(coins, i, N-coins[i-1]);
-        
+        if (i <= 0 && N >= 1)
+            return 0;
+
+        return coinChnage(coins, i - 1, N) + coinChnage(coins, i, N - coins[i - 1]);
+
     }
 }
