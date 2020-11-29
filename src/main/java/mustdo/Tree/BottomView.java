23632a4053dfaa4eca60287f200559066001278e
mukeshsignal <mukesh.singal@outlook.com>
2020-11-29 11:26:19 +0530

    Update BottomView.java

--- src/main/java/mustdo/Tree/BottomView.java
+++ src/main/java/mustdo/Tree/BottomView.java
@@ -1,35 +1,77 @@
 package mustdo.Tree;
 
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+
 
 public class BottomView extends TreeUtil{
-    public static void main(String[] args) {
-        TreeMap<Integer, LinkedList<Node>> map = new TreeMap<>();
-        doVerticalOrderTraversal(getBianryTreeThree(), 0, map);
-        for (Map.Entry<Integer, LinkedList<Node>> e :
-                map.entrySet()) {
-            LinkedList<Node> nodes = e.getValue();
-            System.out.println(nodes.get(0).key + " ");
+    public static class Node {
+        int data;
+        int hd;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.hd = Integer.MAX_VALUE;
         }
+
     }
 
-    static  void doVerticalOrderTraversal(Node root, int hDistance, TreeMap<Integer, LinkedList<Node>> map) {
-        if(root == null){
-            return;
-        }
-        doVerticalOrderTraversal(root.left, hDistance -1, map);
-        doVerticalOrderTraversal(root.right, hDistance +1, map);
-        if(map.containsKey(hDistance)) {
-            map.get(hDistance).add(root);
-        }
-        else {
-            LinkedList<Node> nodes = new LinkedList<>();
-            nodes.add(root);
-            map.put(hDistance, nodes);
-        }
+
+    public static void main(String[] args) {
+        Node root = new Node(20);
+        root.left = new Node(8);
+        root.right = new Node(22);
+        root.right.right = new Node(25);
+        root.left.left = new Node(5);
+        root.left.right = new Node(3);
+        root.left.right.left = new Node(10);
+        root.right.left = new Node(27);
+        root.left.right.right = new Node(14);
+
+        System.out.println((new BottomView()).bottomView(root));
+
     }
+
+
+    public ArrayList <Integer> bottomView(Node root) {
+
+        ArrayList<Integer> array = new ArrayList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        root.hd = 0;
+        queue.add(root);
+
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+
+                map.put(node.hd, node.data);
+                if (node.left != null) {
+                    node.left.hd = node.hd - 1;
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    node.right.hd = node.hd + 1;
+                    queue.add(node.right);
+                }
+            }
+
+        }
+
+        for (Map.Entry<Integer, Integer> e :map.entrySet()) {
+            array.add(e.getValue());
+        }
+
+        return array;
+    }
+
+
 
 
 }
