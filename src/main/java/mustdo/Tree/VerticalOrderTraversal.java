a6931405f5b5c06a9c737d30ea1c902d88d41c31
mukeshsignal <mukesh.singal@outlook.com>
2020-11-29 11:42:09 +0530

    Update VerticalOrderTraversal.java

--- src/main/java/mustdo/Tree/VerticalOrderTraversal.java
+++ src/main/java/mustdo/Tree/VerticalOrderTraversal.java
@@ -1,35 +1,78 @@
 package mustdo.Tree;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
 
 public class VerticalOrderTraversal extends TreeUtil {
     public static void main(String[] args) {
+
+        (new VerticalOrderTraversal()).printVerticalOrder(getBinaryTree2());
+
+
+    }
+
+    private void printVerticalOrder(Node root) {
+
         TreeMap<Integer, LinkedList<Node>> map = new TreeMap<>();
-        doVerticalOrderTraversal(getBinaryTree2(), 0, map);
-        for (Map.Entry<Integer, LinkedList<Node>> e :
-                map.entrySet()) {
+
+        doVOT(root, 0, map);
+
+        for (Map.Entry<Integer, LinkedList<Node>> e : map.entrySet()) {
+
             LinkedList<Node> nodes = e.getValue();
-            for (Node n :
-                    nodes) {
+
+            for (Node n : nodes) {
                 System.out.print(n.key + " ");
             }
             System.out.println();
         }
-
     }
 
-    static void doVerticalOrderTraversal(Node root, int hdistance, TreeMap<Integer, LinkedList<Node>> map) {
+    private void doVOT(Node root, int hDistance, TreeMap<Integer, LinkedList<Node>> map) {
         if (root == null) {
             return;
         }
-        doVerticalOrderTraversal(root.left, hdistance - 1, map);
-        doVerticalOrderTraversal(root.right, hdistance + 1, map);
-        if (map.containsKey(hdistance)) {
-            map.get(hdistance).add(root);
-        } else {
-            LinkedList<Node> nodes = new LinkedList<>();
-            nodes.add(root);
-            map.put(hdistance, nodes);
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(root, 0));
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for(int i = 0; i < size; i++) {
+                Pair pair = queue.poll();
+                Node node = pair.node;
+                int hd = pair.hDistance;
+
+                if(map.containsKey(hd)) {
+                    map.get(hd).add(node);
+                }
+                else {
+                    LinkedList<Node> list = new LinkedList<>();
+                    list.add(node);
+                    map.put(hd, list);
+                }
+
+                if(node.left != null) {
+                    queue.add(new Pair(node.left, hd - 1));
+                }
+                if(node.right != null) {
+                    queue.add(new Pair(node.right, hd + 1));
+                }
+            }
+        }
+    }
+
+    class Pair {
+        Node node;
+        int hDistance;
+
+        Pair(Node node, int hDistance) {
+            this.node = node;
+            this.hDistance = hDistance;
+
         }
     }
 }
