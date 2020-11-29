82951e4835a2199ec631105be102985f97efec8d
mukeshsignal <mukesh.singal@outlook.com>
2020-11-29 10:26:44 +0530

    Refacored Left View Code on Binary Tree

--- src/main/java/mustdo/Tree/LeftView.java
+++ src/main/java/mustdo/Tree/LeftView.java
@@ -6,31 +6,24 @@ import java.util.Queue;
 public class LeftView extends TreeUtil {
 
     void levelOrderTraversal(Node root) {
-        if (root == null) {
+
+        if(root == null) {
             System.out.println("Tree is empty");
             return;
         }
+
         Queue<Node> queue = new LinkedList<>();
         queue.add(root);
 
-        while (true) {
-            int nodeCount = queue.size();
-            if (nodeCount <= 0)
-                break;
-            boolean flag = true;
-            while (nodeCount > 0) {
+        while (!queue.isEmpty()) {
+
+            int noOfElementsInCurrentLevel = queue.size();
+            System.out.println(queue.peek());
+
+            for(int i = 0; i < noOfElementsInCurrentLevel; i++) {
                 Node node = queue.poll();
-                if (flag) {
-                    System.out.println("Node " + node.key);
-                    flag = !flag;
-                }
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
-                nodeCount--;
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
             }
         }
     }
